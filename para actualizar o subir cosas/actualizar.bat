27c16eb40f1262a7fa7a43d38f29554aeb2ff56f
nico2 <nicostarscanizarez@gmail.com>
2025-08-08 20:42:10 -0300

    actualizar

--- para actualizar o subir cosas/actualizar.bat
+++ para actualizar o subir cosas/actualizar.bat
@@ -1,4 +1,29 @@
 @echo off
+cls
+
+echo ================================
+echo       INICIO DE ACTUALIZACION      
+echo ================================
+
 cd /d "%~dp0..\"
+
+if exist ".git\index.lock" (
+    echo ================================
+    echo  Archivo index.lock detectado  
+    echo  Eliminando para evitar bloqueos
+    echo ================================
+    del ".git\index.lock"
+)
+
+echo.
+echo ================================
+echo          Haciendo git pull        
+echo ================================
 git pull
+
+echo.
+echo ================================
+echo       ACTUALIZACION FINALIZADA      
+echo ================================
+
 pause
