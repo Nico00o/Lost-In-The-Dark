7dc097fede026bc08fcc5e2ad42b84619895973d
nico2 <nicostarscanizarez@gmail.com>
2025-08-09 19:40:54 -0300

    2025-08-09 19:40:27 - actualizacion del movimiento del personaje, y tilemap

--- game/escenarios/escenario_1/parallax/camera_2d.gd
+++
@@ -1,9 +0,0 @@
-extends Camera2D
-
-var velocidad = 40
-
-func _process(delta):
-	if Input.is_action_pressed("ui_right"):
-		position.x += velocidad * delta
-	if Input.is_action_pressed("ui_left"):
-		position.x -= velocidad * delta
--- game/personajes/principales/joseph.gd
+++ game/personajes/principales/joseph.gd
@@ -1,24 +1,55 @@
-extends CharacterBody2D 
+extends CharacterBody2D
 
-@export var velocidad_mov : float
-@onready var  animate_sprite = $AnimatedSprite2D
+@export var velocidad_mov : float = 200.0
+@export var gravedad : float = 900.0
+@export var fuerza_salto : float = 400.0
+@export var step_height : int = 8  # altura máxima que puede subir automáticamente
+
+@onready var animate_sprite = $AnimatedSprite2D
 var is_facing_right = true 
+
 func update_animations():
-	if velocity.x:
+	if velocity.x != 0:
 		animate_sprite.play("caminar")
 	else:
 		animate_sprite.play("reposo")
 
 func _physics_process(delta):
+	# Aplicar gravedad
+	if not is_on_floor():
+		velocity.y += gravedad * delta
+	else:
+		velocity.y = 0
+
 	move_x()
+	saltar()
 	flip()
+
+	# Intentar moverse con auto-step
+	auto_step_move(delta)
+
 	update_animations()
+
+func auto_step_move(delta):
+	var original_position = position
 	move_and_slide()
+
+	if is_on_wall() and is_on_floor():
+		for i in range(1, step_height + 1):
+			position = original_position + Vector2(0, -i)  # Subir i px
+			move_and_slide()
+			if not is_on_wall():
+				break
+
 func flip():
-	if  (is_facing_right and velocity.x < 0) or (not is_facing_right and velocity.x > 0 ):
+	if (is_facing_right and velocity.x < 0) or (not is_facing_right and velocity.x > 0):
 		scale.x *= -1
 		is_facing_right = not is_facing_right
-	
+
 func move_x():
-	var input_axis = Input .get_axis("derecha","izquerda")
+	var input_axis = Input.get_axis("izquierda", "derecha")
 	velocity.x = input_axis * velocidad_mov
+
+func saltar():
+	if Input.is_action_just_pressed("saltar") and is_on_floor():
+		velocity.y = -fuerza_salto
