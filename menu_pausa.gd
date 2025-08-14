1a31c3b9f3581e8b139709b343e0ffdc274985f1
Nico <schrrrongerrr@gmail.com>
2025-08-14 11:50:20 -0300

    2025-08-14 11:49:59 - correccion de errores

--- menu_pausa.gd
+++ menu_pausa.gd
@@ -1,22 +1,29 @@
-extends Node2D
+extends CanvasLayer
 
-@onready var pausa: Node2D = $"."
+func _ready():
+	visible = false
 
-func _input(_event):
-	if Input.is_action_just_pressed("Pause") and get_tree().paused == false:
-		get_tree().paused = true
-		$".".visible = true
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+func _input(event):
+	if event.is_action_pressed("Pause"):
+		if not get_tree().paused:
+			get_tree().paused = true
+			visible = true
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		else:
+			_continuar()
 
-func _on_continuar_pressed():
+func _continuar():
 	get_tree().paused = false
-	$".".visible = false
+	visible = false
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
+func _on_continuar_pressed():
+	_continuar()
+
 func _on_senu_pressed():
-	get_tree().change_scene("res://game/menus/menu_principal/control.tscn")
 	get_tree().paused = false
+	get_tree().change_scene_to_file("res://game/menus/menu_principal/control.tscn")
 
 func _on_salir_pressed():
-	get_tree().quit()
 	get_tree().paused = false
+	get_tree().quit()
