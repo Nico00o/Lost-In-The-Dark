c1901e758cd47d23ecdcafb47e6a16f10e988778
Guaimas <samuelguaimas907@gmail.com>
2025-09-04 09:40:33 -0300

    2025-09-04 09:40:07 - Agregado para poder hacer dialogos y primer dialogo en un cartel

--- game/escenarios/escenario_1/tiles/cartel_dialogo.gd
+++ game/escenarios/escenario_1/tiles/cartel_dialogo.gd
@@ -1,15 +1,29 @@
 extends Area2D
-@export var dialogo : DialogueResource
-var esta_cerca := false
 
-func _on_body_entered(body):
-	if body.is_in_group("Player"):
-		esta_cerca = true
+@onready var exclamation_mark = $ExclamationMark
+const CARTEL = preload("res://game/Dialogo/Cartel.dialogue")
+var is_player_close = false
+var is_dialogue_active = false
 
-func _on_body_exited(body):
-	if body.is_in_group("Player"):
-		esta_cerca = false
+func _ready():
+	DialogueManager.dialogue_started.connect(_on_dialogue_started)
+	DialogueManager.dialogue_ended.connect(_on_dialogue_ended)
 
 func _process(delta):
-	if esta_cerca and Input.is_action_just_pressed("interact"):
-		DialogueManager.show_example(dialogo, "Cartel")
+	if is_player_close and Input.is_action_just_pressed("interact") and not is_dialogue_active:
+		DialogueManager.show_dialogue_balloon(CARTEL, "start")
+
+func _on_area_entered(area):
+	exclamation_mark.visible = true
+	is_player_close = true
+
+func _on_area_exited(area):
+	exclamation_mark.visible = false
+	is_player_close = false
+
+func _on_dialogue_started(dialogue):
+	is_dialogue_active = true
+
+func _on_dialogue_ended(dialogue):
+	is_dialogue_active = false
+	await get_tree().create_timer(0.2).timeout
